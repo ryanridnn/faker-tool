6e5d48019f1d000cfac681ff791d0fceb33cb28b
ryanridnn <ryanridnn@gmail.com>
2023-08-17 23:24:35 +0700

    bit more advance templating

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
-import { faker as fakergen } from "@faker-js/faker";
+import { faker } from "@faker-js/faker";
 
-console.log(fakergen);
+console.log(faker);
 // Object.keys(faker).forEach((item) => {
 //   const category = faker[item];
 
@@ -17,31 +17,74 @@ console.log(fakergen);
 
 function App() {
   const [count, setCount] = useState(0);
-  const [script, setScript] = useState("");
+  const [script, setScript] = useState("// 5\nname: name.firstName");
   const [generate, setGenerate] = useState([]);
+  const [blocks, setBlocks] = useState([]);
 
   const onRun = () => {
     try {
-      let faker = fakergen;
-      const repeat = (count, cb) => {
-        const res = [];
+      const res = processScript();
 
-        Array.from({ length: count }).forEach((_, index) => {
-          const cbRes = cb(index);
-          res.push(cbRes);
+      if (res) {
+        const { total, props } = res;
+
+        let newBlocks = [];
+
+        Array.from({ length: total }).forEach((_) => {
+          const block = {};
+
+          props.forEach((prop) => {
+            const paths = prop.value.split(".");
+
+            const func = faker[paths[0]][paths[1]];
+
+            if (func) {
+              block[prop.key] = func();
+            }
+          });
+
+          newBlocks.push(block);
         });
 
-        setGenerate(res);
-      };
-
-      const setInputs = (arr) => {
-        setGenerate(arr);
-      };
-
-      const res = eval(script);
-      console.log(res);
+        setBlocks(newBlocks);
+        console.log(newBlocks);
+      }
     } catch (e) {
       console.log(e);
+    }
+  };
+
+  const firstRegex = /\/\/\s(\d)/;
+  const propRegex = /(\w+)\:(.+)/;
+
+  const processScript = () => {
+    const splitted = script.split("\n");
+
+    const firstLine = splitted[0];
+
+    const matchLine = firstLine.match(firstRegex);
+
+    if (matchLine) {
+      const total = Number(matchLine[1]);
+      const props = [];
+
+      splitted.slice(1).forEach((item) => {
+        const matchItem = item.match(propRegex);
+
+        if (matchItem) {
+          props.push({
+            key: matchItem[1].trim(),
+            value: matchItem[2].trim(),
+          });
+        }
+      });
+
+      return {
+        total,
+        props,
+      };
+    } else {
+      return false;
     }
   };
 
@@ -65,13 +108,18 @@ function App() {
       >
         Run
       </button>
-      <div className="mt-8 flex flex-col gap-2">
-        {generate.map((line, lineIndex) => (
-          <div key={lineIndex} className="">
-            <input
-              value={line}
-              className="w-full p-2 bg-slate-100 rounded-md"
-            />
+      <div className="mt-8 flex flex-col gap-4">
+        {blocks.map((block, blockIndex) => (
+          <div key={blockIndex} className="flex flex-col gap-2 py-1">
+            {Object.keys(block).map((item) => (
+              <div key={item} className="flex items-center gap-4">
+                <div className="">{item}</div>
+                <input
+                  value={block[item]}
+                  className="w-full p-2 bg-slate-100 rounded-md"
+                />
+              </div>
+            ))}
           </div>
         ))}
       </div>
